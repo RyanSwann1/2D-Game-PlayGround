ab8000647df3d98bf511085fb436dfdc46ec9906
Ryan Swann <RyanSwann1992@gmail.com>
2017-08-24 11:23:44 +0100

    Adding files.

--- 2DGamePlayground/src/main.cpp
+++ 2DGamePlayground/src/main.cpp
@@ -1,10 +1,31 @@
 
 #include <string>
 #include <iostream>
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
 
 int main()
 {
 	std::cout << "Hello World.\n";
+
+	sf::Window window(sf::VideoMode(640, 480), "SFML_Window");
+	while (window.isOpen())
+	{
+		sf::Event sfmlEvent;
+		while (window.pollEvent(sfmlEvent))
+		{
+			switch (sfmlEvent.type)
+			{
+			case sf::Event::Closed:
+			{
+				window.close();
+				break;
+			}
+			}
+		}
+
+	}
+
 
 	char c;
 	std::cin >> c;
